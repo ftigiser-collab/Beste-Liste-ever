21a7df055a5f42bc6d8430f2523ea6016c98e380
ftigiser-collab <f.tigiser@gmail.com>
2026-09-19 19:11:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service Worker — beste-liste
    Update: VERSION hochzählen, dann index.html hochladen. */
-const VERSION = 'beste-liste-v14';
+const VERSION = 'beste-liste-v17';
 const CORE = [
   './',
   './index.html',
